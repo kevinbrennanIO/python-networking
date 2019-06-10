0add2e5cb5014e718d27d394af112e3c4c827307
kevinbrennanIO <kevdj2000@gmail.com>
2019-06-10 06:51:52 +0100

    udating basics

--- user_input.py
+++
@@ -1,6 +0,0 @@
-user_says = input("Please enter the string your want to print: ")
-
-print(user_says)
-
-
-print(id(user_says))
---
+++ variables.py
@@ -0,0 +1,5 @@
+#Defining a variable
+
+my_var = 10 #type integer
+my_var = "Hello" #type string
+my_var = True #type boolean
