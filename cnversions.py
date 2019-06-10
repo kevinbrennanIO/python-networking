4fa0d9aab4ee254a6996edc85ba3c7ef718666a9
kevinbrennanIO <kevdj2000@gmail.com>
2019-06-10 22:14:41 +0100

    added more files

---
+++ cnversions.py
@@ -0,0 +1,12 @@
+#Conversions between data types
+
+str() #converting to a string
+int() #converting to an integer
+float() #converting to a float
+list() #converting to a list
+tuple() #converting to a tuple
+set() #converting to a set
+bin() #converting to a binary representation
+hex() #converting to a hexadecimal representation
+int(variable, 2) #converting from binary back to decimal
+int(variable, 16) #converting from hexadecimal back to decimal
