7c009985541ec8b72f040aa28aa9420a55dd6103
kevinbrennanIO <kevdj2000@gmail.com>
2019-06-12 06:40:19 +0100

    adding more files

---
+++ modules.py
@@ -0,0 +1,13 @@
+#Modules and importing - Basics
+import sys #importing the sys module; the import statements should be placed before any other code in your application
+ 
+from math import pi #importing only a variable (pi) from the math module
+ 
+from math import sin #importing only a function (sin()) from the math module; there's no need to add the parantheses of the function when importing it
+ 
+from math import * #importing all the names (variables and functions) 
+
+#Helper functions 
+
+dir(<module_name>)
+help(<module_name>)
